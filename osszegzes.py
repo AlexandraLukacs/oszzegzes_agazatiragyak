2977e58ce2c94865d21f09f2a1efa43408987d91
AlexandraLukacs <lszandika15@gmail.com>
2024-01-16 10:17:26 +0100

    jav

--- osszegzes.py
+++ osszegzes.py
@@ -1,12 +1,11 @@
 def osszegzes_fel():
+    n: int= 0
+    i: int = 1
     ossz: int= 0
-    i: int =1
-    n: int= int(input("N="))
-    while n < 0:
-        for i in range(0, n, 1):
-            if i > n:
-                ossz += i
-        i += 1
+    while (n <= 0):
+        n: int= int(input("N="))
+    for i in range(0, n+1):
+        ossz += i
     print(f"Az első {n} db természetes szám összege: {ossz}")
 
         
